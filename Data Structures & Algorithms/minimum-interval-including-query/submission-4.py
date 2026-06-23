f28882e478ed48d24dd078e1e0c6f21e17ebc33f
Arkaprava De <arkaprava08@gmail.com>
2026-06-22 23:50:19 -0700

    Add: minimum-interval-including-query - submission-4

---
+++ Data Structures & Algorithms/minimum-interval-including-query/submission-4.py
@@ -0,0 +1,21 @@
+class Solution:
+    def minInterval(self, intervals: List[List[int]], queries: List[int]) -> List[int]:
+        MAX=999999
+        tmp=[MAX]*10001
+
+        for interval in intervals:
+            gap=(interval[1]-interval[0])+1
+            for i in range(interval[0], interval[1]+1):
+                tmp[i]=min(tmp[i], gap)
+
+        out=[]
+        for query in queries:
+            if tmp[query] != MAX:
+                out.append(tmp[query])
+            else:
+                out.append(-1)
+
+        # print(out)
+        return out
+            
+        
